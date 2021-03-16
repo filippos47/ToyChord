d443f4ace543f2c9490e0ac7af3330c7b1894bb5
KosPsych <kwstaspsychogios13@hotmail.com>
2021-03-16 20:14:48 +0200

    ip_list -> ip_string

--- cli-client/cli/commands/bulk_operations.py
+++ cli-client/cli/commands/bulk_operations.py
@@ -14,16 +14,24 @@ from random import randrange
 @click.option('--source_file', '-f', type = str, required = True,
         help = 'File which contains desired operations, one per line. Format' \
                ' should be `<OPERATION>, [<KEY>], [<VALUE>]`')
+
+@click.option('--addr_string', '-addr_s', type = str, required = True)
+
 @global_options
 @click.pass_context
-def bulk_operations(ctx, ip_port_list, source_file): #[(ip1,port1),(ip2,port2) ] 
+def bulk_operations(ctx,source_file,addr_string): # ip1:port1,ip2:port2...(string)
     with open(source_file, "r") as fp:
         for line in fp.readlines():
             arguments = [ x.strip() for x in line.split(',') ]
             command = arguments[0]
-            random_node=ip_port_list[randrange(len(ip_port_list))]
+
+            addr_list=addr_string.split(",") 
+            for i in range(len(addr_list)):
+                  addr_list[i]=addr_list[i].split(":")
+            random_node=addr_list[randrange(len(addr_list))]
             random_ip=random_node[0]
             random_port=random_node[1]
+
             if command == "join":
                 ctx.invoke(join, ip_address = random_ip,port=random_port)
             elif command == "depart":
