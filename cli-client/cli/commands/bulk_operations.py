0a290e54445a1539d3dd6c22e4aaff2af04000da
KosPsych <kwstaspsychogios13@hotmail.com>
2021-03-16 14:45:55 +0200

    small change

--- cli-client/cli/commands/bulk_operations.py
+++ cli-client/cli/commands/bulk_operations.py
@@ -8,6 +8,7 @@ from .overlay import overlay
 from .insert import insert
 from .delete import delete
 from .query import query
+from random import randrange
 
 @click.command()
 @click.option('--source_file', '-f', type = str, required = True,
@@ -15,23 +16,24 @@ from .query import query
                ' should be `<OPERATION>, [<KEY>], [<VALUE>]`')
 @global_options
 @click.pass_context
-def bulk_operations(ctx, ip_address, port, source_file):
+def bulk_operations(ctx, ip_port_list, source_file): #[(ip1,port1),(ip2,port2) ] 
     with open(source_file, "r") as fp:
         for line in fp.readlines():
             arguments = [ x.strip() for x in line.split(',') ]
             command = arguments[0]
+            random_node=randrange(len(ip_port_list))
             if command == "join":
-                ctx.invoke(join, ip_address = ip_address, port = port)
+                ctx.invoke(join, ip_address = ip_port_list[random_node][0], port = ip_port_list[random_node][0][1])
             elif command == "depart":
-                ctx.invoke(depart, ip_address = ip_address, port = port)
+                ctx.invoke(depart, ip_address = ip_port_list[random_node][0], port = ip_port_list[random_node][0][1])
             elif command == "overlay":
-                ctx.invoke(overlay, ip_address = ip_address, port = port)
+                ctx.invoke(overlay, ip_address = ip_port_list[random_node][0], port = ip_port_list[random_node][0][1])
             elif command == "insert":
-                ctx.invoke(insert, ip_address = ip_address, port = port,
+                ctx.invoke(insert, ip_address = ip_port_list[random_node][0], port = ip_port_list[random_node][0][1],
                         key = arguments[1], value = arguments[2])
             elif command == "delete":
-                ctx.invoke(delete, ip_address = ip_address, port = port,
+                ctx.invoke(delete, ip_address = ip_port_list[random_node][0], port = ip_port_list[random_node][0][1],
                         key = arguments[1])
             elif command == "query":
-                ctx.invoke(query, ip_address = ip_address, port = port,
+                ctx.invoke(query, ip_address = ip_port_list[random_node][0], port = ip_port_list[random_node][0][1],
                         key = arguments[1])
